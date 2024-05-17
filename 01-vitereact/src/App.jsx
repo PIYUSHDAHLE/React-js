cdf35370cf3f4e0b871d380f5bfb97bc629bc0d3
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-17 19:51:46 +0530

    how to create component in vite

---
+++ 01-vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Boss from "./Boss"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Boss/>
+    <h1>Chai aur react with vite | Piyush Dahle</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01-vitereact/src/Boss.jsx
@@ -0,0 +1,10 @@
+function Boss(){
+    return(
+        <>
+        <h1>Hi, I am Boss</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates reprehenderit perspiciatis laborum alias exercitationem molestiae ipsam obcaecati culpa porro eaque? At adipisci impedit fugiat quia eligendi vero provident minus excepturi?</p>
+        </>
+    );
+}
+
+export default Boss
